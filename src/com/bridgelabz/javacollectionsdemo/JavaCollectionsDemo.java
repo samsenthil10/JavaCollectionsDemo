3a114ab203dadecac0622695604e053c8b07d632
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-13 03:48:26 +0530

    Added: To demonstrate stack

--- src/com/bridgelabz/javacollectionsdemo/JavaCollectionsDemo.java
+++ src/com/bridgelabz/javacollectionsdemo/JavaCollectionsDemo.java
@@ -3,26 +3,44 @@ package com.bridgelabz.javacollectionsdemo;
 import java.util.*;
 
 public class JavaCollectionsDemo {
-	
+
 	public static void main(String[] args) {
-		
+
 		doListDemo();
+		doStackDemo();
 	}
-	
+
 	public static void doListDemo() {
-		
+
 		List<String> list = new LinkedList<>();
-		
-		System.out.println("Inside List Demo");
+
+		System.out.println("\nInside List Demo\n");
 		list.add("Ravi");
 		list.add("Vijay");
 		list.add("Ravi");
 		list.add("Ajay");
-		
+
 		Iterator<String> iterator = list.iterator();
 		while(iterator.hasNext()) {
 			System.out.println(iterator.next());
 		}
 	}
 
+	public static void doStackDemo() {
+
+		Stack<String> stack = new Stack<>();
+
+		System.out.println("\nInside Stack Demo\n");
+		stack.push("Ayush");
+		stack.push("Garvit");
+		stack.push("Amrit");
+		stack.push("Ashish");
+		stack.push("Garima");
+		String popped = stack.pop();
+
+		Iterator<String> iterator = stack.iterator();
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+	}
 }
